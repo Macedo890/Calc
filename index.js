045154eff7c20aa21a34aac3a718776f0d2149ac
Macedo890 <macedoprofissional890@gmail.com>
2024-07-25 19:42:51 -0300

    3v

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ let buttons = document.querySelectorAll('.btn');
 let calc = document.querySelector('#calc');
 let texto = document.querySelector('#texto');
 
+let operator;
+let numbers = [];
+
+function calculate(n1, operator, n2) {
+    let result = '';
+
+    if (operator === 'some') {
+        result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator === 'sub') {
+        result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator === 'multi') {
+        result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2);
+    }
+
+    return result;
+}
+
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         let valor = button.value;
@@ -20,7 +39,6 @@ buttons.forEach((button) => {
             });
 
             calc.dataset.previousKeyType = 'number';
-            console.log('number key');
         } else if (
             action === 'factore' ||
             action === 'divide' ||
@@ -33,13 +51,22 @@ buttons.forEach((button) => {
             button.classList.add('is-depressed');
             calc.dataset.previousKeyType = 'operator';
 
+            if (action !== 'equal' && action !== 'decimal') {
+                operator = action;
+                numbers[0] = texto.value;
+                texto.value = '';
+            }
+
             if (action === 'decimal') {
                 texto.value = texto.value + valor;
             }
 
-            console.log(`Operator Key ${action}`);
+            if (action === 'equal') {
+                const secondValue = texto.value;
+                numbers[1] = secondValue;
+                texto.value = calculate(numbers[0], operator, numbers[1]);
+                operator = undefined;
+            }
         }
-
-        console.log(valor);
     });
 });
